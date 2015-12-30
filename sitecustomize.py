3cad3fffd479be87e94b8f58747fca7f91e45505
jan@pidgin <jan@esserjan.de>
2015-12-30 15:57:51 +0100

    Python 2.x backport

---
+++ sitecustomize.py
@@ -0,0 +1,6 @@
+# see http://www.ianbicking.org/illusive-setdefaultencoding.html 
+# PYTHONPATH needs to be set to this directory, like for example "export PYTHONPATH=."
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
